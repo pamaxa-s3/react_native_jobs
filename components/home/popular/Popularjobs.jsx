6a43cf29a380b556d4b962cd2ed082c0e1d68908
Roman <pamaxa-s3@ukr.net>
2023-04-03 20:53:34 +0300

    add PopularJob & PopularJobCard

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -6,10 +6,15 @@ import styles from './popularjobs.style'
 import { COLORS, SIZES } from '../../../constants'
 import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 
+import useFetch from '../../../hook/useFetch'
+
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = true;
-  const error = false;
+
+  const { data, isLoading, error } = useFetch('search', {
+    query: 'React developer',
+    num_pages: 1,
+  });
 
   return (
     <View style={styles.container}>
@@ -27,7 +32,7 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
+            data={[1, 2, 3, 4, 5, 6, 7, 8]}
             renderItem={({ item }) => {
               <PopularJobCard
                 item={item}
---
+++ hook/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (endpoint, query) => {
+	const [data, setData] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	const axios = require("axios");
+
+	const options = {
+		method: 'GET',
+		url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+		headers: {
+			'X-RapidAPI-Key': 'e0edc206a6msh9bb08e07ec003e0p157b79jsnf3a04f125bb1',
+			'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+		},
+		params: { ...query },
+	};
+
+	const fetchData = async () => {
+		setIsLoading(true);
+		try {
+			const response = await axios.request(options);
+
+			setData(response.data.data);
+			setIsLoading(false);
+		} catch (error) {
+			setError(true);
+			alert('There is an error!')
+		} finally {
+			setIsLoading(false);
+		}
+	}
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const refetch = () => {
+		setIsLoading(true);
+		fetchData();
+	}
+
+	return { data, isLoading, error, refetch };
+}
+
+export default useFetch;
